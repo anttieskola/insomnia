4afdc3ba5ce244e9c96743565514a5c8565ad524
Antti Eskola <antti.eskola@gmail.com>
2014-11-12 14:02:10 +0100

    Missing file. Oh why you VS miss my files...

---
+++ insomnia.Domain/RequestEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insomnia.Domain
+{
+    public class RequestEngine : IRequestEngine
+    {
+        public async Task<int> Create(String url)
+        {
+            return await RequestDaemon.Instance.Create(url);
+        }
+
+        public IEnumerable<RequestModel> List()
+        {
+            return RequestDaemon.Instance.List();
+        }
+    }
+}
